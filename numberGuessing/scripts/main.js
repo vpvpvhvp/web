898f56b773f238e9d7435a2cc1e8cb011fcbc72e
vpvpvhvp@hanmail.net <vpvpvhvp@hanmail.net>
2018-03-18 21:33:20 +0900

    1

---
+++ numberGuessing/scripts/main.js
@@ -0,0 +1,52 @@
+alert('hello!');
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector('guesses');
+var lastResult = document.querySelector('lastResult');
+var lowOrHi = document.querySelector('lowOrHi');
+
+var guessSubmit = document.querySelector('guessSubmit');
+var guessField = document.querySelector('guessField');
+
+var guessCount = 1;
+var resetButton;
+
+if(guessSubmit){
+  alert('guessing...');
+	guessSubmit.addEventListener('click',checkGuess);
+}
+
+function checkGuess() {
+  var userGuess = Number(guessField.value);
+
+  if (guessCount === 1) {
+  	alert('guess!');
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
+ 
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+ 
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+alert('bye!');
